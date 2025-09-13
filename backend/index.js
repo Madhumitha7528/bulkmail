46de5de9f4b7b8765464b4cc54e1014de5335757
Madhumitha7528 <madhumitha7528@gmail.com>
2025-09-13 19:53:12 +0530

    Update Project

---
+++ backend/index.js
@@ -0,0 +1,69 @@
+const express = require("express")
+const cors = require("cors")
+const nodemailer = require("nodemailer")
+const mongoose = require("mongoose")
+
+require("dotenv").config();
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+.then(function(){
+    console.log("Connected to DB")
+})
+.catch(function(){
+    console.log("Failed to connect to DB")
+})
+
+const credential = mongoose.model("credential",{},"bulkmail")
+
+app.post("/sendemail",function(req,res){
+
+    var msg = req.body.msg
+    var emailList = req.body.emailList
+
+    credential.find()
+    .then(function(data){
+    const transporter = nodemailer.createTransport({
+    service:"gmail",
+    auth: {
+        user: data[0].toJSON().user,
+        pass: data[0].toJSON().pass,
+    },
+    });
+    new Promise(async function(resolve,reject){
+        try{
+        for(var i=0;i<emailList.length;i++){
+        await transporter.sendMail({
+        from:"kavimadhu777@gmail.com",
+        to:emailList[i],
+        subject:"A message from BulkMail App",
+        text:msg
+    }
+    )
+    console.log("Email sent to:"+emailList[i])
+    }
+    resolve("Success")
+    }
+    catch(error){
+        reject("Failed")
+    }
+    })
+    .then(function(){
+        res.send(true)
+    })
+    .catch(function(){
+        res.send(false)
+    })
+})
+.catch(function(error){
+    console.log(error)
+})
+})
+
+app.listen(5000,function(){
+    console.log("Server Started...")
+})
